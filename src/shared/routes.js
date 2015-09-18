7773eee5432ade109f166a52afaa2fbf997d6935
Rob Retchless <retchles@ca.ibm.com>
2015-09-17 23:20:06 -0400

    Add referral form route

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -2,7 +2,11 @@ import { Route } from "react-router";
 import React from "react";
 
 import AppHandler from "./components/AppHandler";
+import ReferralForm from "./components/referralform/referralform"
 
 export default (  
-  <Route handler={ AppHandler } path="/" />
+  <Route>
+  	<Route handler={ ReferralForm } path="/submit" />
+  	<Route handler={ AppHandler } path="/" />
+  </Route>
 );
