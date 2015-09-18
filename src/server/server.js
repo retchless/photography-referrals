25bae02e100bf8e8dd5503b2176dc0df36e1a86a
Adam Archer <agarcher@ca.ibm.com>
2015-09-17 23:44:26 -0400

    Add mailer utility and sample /sendMail route

--- src/server/server.js
+++ src/server/server.js
@@ -2,12 +2,31 @@ import express from "express";
 import React from "react";  
 import Router from "react-router";
 import ReactViews from "express-react-views";
+import mailer from "./utils/mailer";
 const app = express();
 
 // set up Jade
 app.set('views', './views');  
 app.set('view engine', 'jsx');
 app.engine('jsx', ReactViews.createEngine());
+
+app.get("/sendMail", function(req, res) {
+  var address = req.query.address;
+  if (!address) {
+  	res.status = 400;
+  	res.end("Missing required url parameter: address");
+  	return;
+  }
+
+  mailer.sendMail(address, {subject: "Subject line", body: "My mailer is in a module now"}, function(err, callback) {
+    if (err) {
+      res.status = 400;
+      res.end(err.toString());
+      return;
+    }
+    res.end("Mail sent!");
+  })
+});
 
 import routes from "../shared/routes";
 
