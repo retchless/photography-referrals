919a1599c1d7a1c4a4e16ddf25f3ad922a42e8f4
Adam Archer <agarcher@gmail.com>
2015-11-05 00:04:50 -0500

    save referrals to the db on form submit

--- routes/referral.js
+++ routes/referral.js
@@ -12,3 +12,66 @@ exports.get = function(req, res) {
     })
   });
 }
+
+exports.post = function(req, res) {
+  if (!req.body || !req.body.referrer) {
+    res.status(400);
+    res.end("Invalid referral. A referring photographer is required.");
+    return;
+  }
+  if (!req.body.clientFirstName) {
+    res.status(400);
+    res.end("Invalid referral. Client first name is required.");
+    return;
+  }
+  if (!req.body.clientLastName) {
+    res.status(400);
+    res.end("Invalid referral. Client last name is required.");
+    return;
+  }
+  if (!req.body.clientEmail) {
+    res.status(400);
+    res.end("Invalid referral. Client email is required.");
+    return;
+  }
+  if (!req.body.weddingDate) {
+    res.status(400);
+    res.end("Invalid referral. Wedding date is required.");
+    return;
+  }
+  if (!req.body.weddingCity) {
+    res.status(400);
+    res.end("Invalid referral. Wedding city is required.");
+    return;
+  }
+  if (!req.body.weddingVenue) {
+    res.status(400);
+    res.end("Invalid referral. Wedding venue is required.");
+    return;
+  }
+
+  var referral = {
+    referrer: req.body.referrer,
+    client: {
+      fname: req.body.clientFirstName,
+      lname: req.body.clientLastName,
+      email: req.body.clientEmail
+    },
+    wedding: {
+      date: req.body.weddingDate,
+      city: req.body.weddingCity,
+      venue: req.body.weddingVenue
+    },
+    notes: req.body.notes || ""
+  };
+  console.log(referral);
+
+  db.insertReferral(referral, function(err, result) {
+    if (err) {
+      res.status(400);
+      res.end(err.toString());
+      return;
+    }
+    res.end("Referral created successfully!");
+  });
+}
