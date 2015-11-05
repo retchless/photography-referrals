7bcbc903cfcc86435fdbd653162a564dc8503c64
Rob Retchless <retchles@ca.ibm.com>
2015-11-04 19:24:52 -0500

    support openshift

--- src/server/server.js
+++ src/server/server.js
@@ -75,6 +75,9 @@ app.get('/*', function (req, res) {
   });
 });
 
-app.listen(appEnv.port, appEnv.bind, function() {
-    console.log("server starting on " + appEnv.url)
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || appEnv.port || 6001;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || appEnv.bind || "127.0.0.1";
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
